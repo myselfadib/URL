5f7b76aad09449a6c4179d6c9f221aad384f0d8b
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-07-14 20:07:55 +0600

    almost done

--- urlshortner/shortner/urls.py
+++ urlshortner/shortner/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'shortner'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create/', views.create, name='create'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go'),
 ]
--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import uuid
 from .models import Url
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -13,5 +14,8 @@ def create(request):
         new_url = Url(link=link,uuid=uid)
         new_url.save()
         return HttpResponse(uid)
-
     
+def go(request,pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect('https://'+url_details.link)
+
